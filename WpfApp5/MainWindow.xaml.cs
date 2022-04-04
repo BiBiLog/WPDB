cc3fbab583d9ce739473661380a52f348dbea8f3
BiBiLog <dzugaev2002@gmail.com>
2022-04-04 09:24:20 +0300

    first commit

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Login = "admin";
+            var Parol = "admin";
+
+            if ((L.Text == Login) && (P.Password == Parol))
+            {
+                Window neww = new Window1();
+                neww.Show();
+                Close();
+            }
+            if ((L.Text == Login) && (P.Password != Parol))
+            {
+                MessageBox.Show("Не верный пароль");
+            }
+            if (L.Text != Login)
+            {
+                MessageBox.Show("Не верный логин");
+            }
+        }
+    }
+}
---
+++ WpfApp5/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    
+    public partial class Window1 : Window
+    {
+        BDWPFEntities dW = new BDWPFEntities();
+        public Window1()
+        {
+            InitializeComponent();
+            dataGrid.ItemsSource = dW.FIO.ToList();
+        }
+
+
+
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+            
+        {
+            var FioForRemoving = dataGrid.SelectedItems.Cast<FIO>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {FioForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dW.FIO.RemoveRange(FioForRemoving);
+                    
+                    MessageBox.Show("Данные успешно удалены!");
+
+                    dataGrid.ItemsSource = dW.FIO.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
